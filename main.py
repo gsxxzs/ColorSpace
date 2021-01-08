d27ed3f41fc17a51be631a4db7ce8f1b103e5329
sheng guo <51937758+gsxxzs@users.noreply.github.com>
2021-01-08 19:42:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,9 @@
 import cv2
 import torch
 
-
 from ColorSpaceTransform import ColorSpaceTransform
 
 if __name__ == '__main__':
-    # HSV = np.load("LOL/low_HSV/HSV_2.npy")
-    # RGB = cv2.cvtColor(HSV, cv2.COLOR_HSV2BGR)
-    # cv2.imshow("RGB", RGB)
-    # cv2.waitKey(0)
-    # H, S, L = getHLS("778.png")
-    # H1, S1, L1 = getHLS("778_h.png")
-    # d_h = (H - H1)/360
-    # d_S = S - S1
-    # d = (d_h + d_S) / 2
-    # L = L - d
-    # L = L ** (1 / 4.0)
-    # # RGB = numpy.empty([400, 600, 3], dtype=float)
-    # # # RGB = np.ndarray([H, L, S])
-    # # # RGB = RGB.transpose(1, 2, 0)
-    # # RGB[:, :, 0] = H
-    # # RGB[:, :, 1] = L
-    # # RGB[:, :, 2] = S
-    # # RGB = RGB.astype(np.float32)
-    # # RGB = cv2.cvtColor(RGB, cv2.COLOR_HLS2RGB)
-    # cv2.imshow("RGB", L)
-    # cv2.waitKey(0)
 
     img = cv2.imread("42.png")
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
